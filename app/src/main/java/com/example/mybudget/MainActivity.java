8a57460c2944d93cb47f9f6d21fd858d94dcdedb
kyBa-Le <lekyba2000hc@gmail.com>
2025-05-21 20:34:50 +0700

    Complete the welcome page, change the app icon

--- app/src/main/java/com/example/mybudget/MainActivity.java
+++ app/src/main/java/com/example/mybudget/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mybudget;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button nameEntryButton;
+    private EditText nameEntryEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.welcome_page);
+
+        mapping();
+
+        nameEntryButton.setOnClickListener(v -> {
+            String text = nameEntryEditText.getText().toString();
+            Toast.makeText(this, "You've enter " + text, Toast.LENGTH_SHORT).show();
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void mapping() {
+        this.nameEntryButton = findViewById(R.id.buttonEnterName);
+        this.nameEntryEditText = findViewById(R.id.nameEntryEditText);
     }
 }
